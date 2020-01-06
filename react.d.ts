559a8007a24ae6861900de00fdde7d7d3f00a2bd
Daniel Werthen <danielwerthen@gmail.com>
2020-01-06 12:00:29 +0100

    Fix the types

--- react.d.ts
+++ react.d.ts
@@ -1,32 +1,63 @@
-import React from 'react'
-export * from 'react'
+import React, {
+  ReactHTML,
+  ReactSVG,
+  HTMLAttributes,
+  SVGFactory,
+  DOMAttributes,
+  DOMFactory,
+  FunctionComponent,
+  ClassType,
+  ClassicComponent,
+  ComponentState,
+  ClassicComponentClass,
+  ReactElement
+} from "react";
+export * from "react";
 
-export default React
+export default React;
 
-export function StilrenProvider(options: { styletron: any, stylePrefix: string, breakpoints: object, pseudos: string[], extensions: object[] }) {
-}
+export function StilrenProvider(options: {
+  styletron: any;
+  stylePrefix: string;
+  breakpoints: object;
+  pseudos: string[];
+  extensions: object[];
+}): ReactElement;
 
-export function StilrenExtender(options: { children: any, extensions: object[], replace: boolean }) {
-}
+export function StilrenExtender(options: {
+  children: any;
+  extensions: object[];
+  replace: boolean;
+}): ReactElement;
 
-type Extension = {}
+type Extension = {};
 
 export const DECORATE_KEY = Symbol("Key for storing wrapped components");
 
-type Component = keyof ReactHTM
+export function componentFactory(
+  type: keyof ReactHTML,
+  ...extensions: Extension[]
+): FunctionComponent<HTMLElement>;
 
-export function componentFactory<P extends {}>(Component: , ...extensions: Extension[]): React.FC<P>
+export function componentFactory(
+  type: keyof ReactSVG,
+  ...extensions: Extension[]
+): FunctionComponent<SVGElement>;
+export function componentFactory<P extends DOMAttributes<T>, T extends Element>(
+  type: string,
+  ...extensions: Extension[]
+): FunctionComponent<HTMLElement>;
 
-    export function componentFactory<T extends HTMLElement>( type: keyof ReactHTML): HTMLFactory<T>;
-    export function createFactory(
-        type: keyof ReactSVG): SVGFactory;
-    export function createFactory<P extends DOMAttributes<T>, T extends Element>(
-        type: string): DOMFactory<P, T>;
-
-    // Custom components
-    export function createFactory<P>(type: FunctionComponent<P>): FunctionComponentFactory<P>;
-    export function createFactory<P>(
-        type: ClassType<P, ClassicComponent<P, ComponentState>, ClassicComponentClass<P>>): CFactory<P, ClassicComponent<P, ComponentState>>;
-    export function createFactory<P, T extends Component<P, ComponentState>, C extends ComponentClass<P>>(
-        type: ClassType<P, T, C>): CFactory<P, T>;
-    export function createFactory<P>(type: ComponentClass<P>): Factory<P>;
+// Custom components
+export function componentFactory<P>(
+  type: FunctionComponent<P>,
+  ...extensions: Extension[]
+): FunctionComponent<P>;
+export function createFactory<P>(
+  type: ClassType<
+    P,
+    ClassicComponent<P, ComponentState>,
+    ClassicComponentClass<P>
+  >,
+  ...extensions: Extension[]
+): FunctionComponent<P>;
