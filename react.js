8af9019c9cf642ee00f77907d15654841eba33e1
Daniel Werthen <danielwerthen@gmail.com>
2020-01-06 13:31:53 +0100

    It works to decorate the createElement

---
+++ react.js
@@ -0,0 +1,3 @@
+import React from "./dist/react";
+export * from "./dist/react";
+export default React;
---
+++ src/react/index.ts
@@ -0,0 +1,92 @@
+import {
+  createElement as baseCreateElement,
+  createFactory as baseCreateFactory,
+  createContext,
+  createRef,
+  useCallback,
+  useContext,
+  useDebugValue,
+  useEffect,
+  useLayoutEffect,
+  useImperativeHandle,
+  useMemo,
+  useReducer,
+  useRef,
+  useState,
+  cloneElement,
+  forwardRef,
+  isValidElement,
+  lazy,
+  memo
+} from "react";
+
+type createElementType = typeof baseCreateElement;
+
+export const createElement: createElementType = ((
+  type: any,
+  props?: any,
+  ...children: any
+) => {
+  return baseCreateElement(type, props, children);
+}) as createElementType;
+
+type createFactoryType = typeof baseCreateFactory;
+
+export const createFactory: createFactoryType = ((type: any) => {
+  const factory = createElement.bind(null, type) as any;
+  // Expose the type on the factory and the prototype so that it can be
+  // easily accessed on elements. E.g. `<Foo />.type === Foo`.
+  // This should not be named `constructor` since this may not be the function
+  // that created the element, and it may not even be a constructor.
+  // Legacy hook: remove it
+  factory.type = type;
+  return factory;
+}) as createFactoryType;
+
+export default {
+  createElement,
+  createContext,
+  createFactory,
+  createRef,
+  useCallback,
+  useContext,
+  useDebugValue,
+  useEffect,
+  useLayoutEffect,
+  useImperativeHandle,
+  useMemo,
+  useReducer,
+  useRef,
+  useState,
+  cloneElement,
+  forwardRef,
+  isValidElement,
+  lazy,
+  memo
+};
+
+export {
+  createContext,
+  createRef,
+  useCallback,
+  useContext,
+  useDebugValue,
+  useEffect,
+  useLayoutEffect,
+  useImperativeHandle,
+  useMemo,
+  useReducer,
+  useRef,
+  useState,
+  cloneElement,
+  forwardRef,
+  isValidElement,
+  lazy,
+  memo
+};
+
+declare module "react" {
+  interface HTMLAttributes<T> {
+    [key: string]: any;
+  }
+}
