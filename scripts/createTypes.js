a6acbcde3bc4498b7c429bfc511c5abef9d59b4e
Daniel Lichtenstein <danielwerthen@gmail.com>
2020-09-06 12:29:29 +0200

    Make better types

--- scripts/createTypes.js
+++ scripts/createTypes.js
@@ -6,54 +6,52 @@ const recast = require("recast");
 const input = recast.parse(
   fs.readFileSync(path.join(__dirname, "../node_modules/csstype/index.d.ts")),
   {
-    parser: require("recast/parsers/typescript")
+    parser: require("recast/parsers/typescript"),
   }
 );
-const csstypes = input.program.body
+
+const allProps = input.program.body
   .filter(
-    node =>
-      node.declaration && node.declaration.type === "TSTypeAliasDeclaration"
+    (node) =>
+      node.declaration &&
+      node.declaration.id.name.startsWith("Standard") &&
+      node.declaration.id.name.endsWith("handProperties")
   )
-  .map(node => node.declaration)
-  .filter(node => node.id.name.endsWith("Property"))
-  .filter(
-    node =>
-      !node.id.name.startsWith("Ms") &&
-      !node.id.name.startsWith("Moz") &&
-      !node.id.name.startsWith("Webkit")
-  )
-  .map(decl => ({
-    name: decl.id.name,
-    params:
-      (decl.typeParameters &&
-        decl.typeParameters.params.map(param => param.name)) ||
-      []
+  .reduce((sum, node) => [...sum, ...node.declaration.body.body], [])
+  .map((propSig) => ({
+    name: propSig.key.name,
+    type: propSig.typeAnnotation.typeAnnotation.typeName.name,
+    typeArg:
+      propSig.typeAnnotation.typeAnnotation.typeParameters &&
+      propSig.typeAnnotation.typeAnnotation.typeParameters.params[0].typeName
+        .name,
+    comment: propSig.comments[0].value,
   }));
 
-const allProperties = csstypes.map(({ name, params }) => ({
-  propName: humps.camelize(name.substr(0, name.length - "Property".length)),
-  typeName: name,
-  typeInstance: params.length > 0 ? `${name}<${params.join(", ")}>` : name
-}));
+const typeImports = allProps.map((p) => p.type).join(",\n  ");
+const props = allProps.reduce(
+  (sum, prop) => [
+    ...sum,
+    `/*${prop.comment
+      .split("\n")
+      .map((str) => `  ${str}`)
+      .join("\n")}*/`,
+    `$${prop.name}?: ${prop.type}${prop.typeArg ? `<${prop.typeArg}>` : ""},`,
+  ],
+  []
+);
 
-const props = allProperties
-  .flatMap(({ propName, typeInstance }) => `$${propName}?: ${typeInstance}`)
-  .join("\n      ");
-const typeImports = [
-  ...allProperties.reduce((sum, { typeName }) => sum.add(typeName), new Set())
-].join(",\n      ");
-
-const typeDef = `
-  import 'react'
-  import {
-    ${typeImports}
-  } from 'csstype'
-
-  declare module 'react' {
-    interface HTMLAttributes<T> {
-      ${props}
-    }
-  }
-`;
+const typeDef = [
+  'import "react"',
+  "import {",
+  "  " + typeImports,
+  '} from "csstype"',
+  "",
+  'declare module "react" {',
+  "  interface HTMLAttributes<T> {",
+  ...props.map((p) => `    ${p}`),
+  "  }",
+  "}",
+].join("\n");
 
 fs.writeFileSync(path.join(__dirname, "../dist/types.d.ts"), typeDef);
