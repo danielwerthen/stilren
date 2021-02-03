c885c0ee41a0f8a135e54496f3cbf20396de5299
Daniel Lichtenstein <danielwerthen@gmail.com>
2021-02-03 17:12:28 +0100

    Just wrap react

--- stories/Thing.stories.tsx
+++ stories/Thing.stories.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
-import { Thing, Props } from '../src';
+
+function Thing() {
+  return <p>Test</p>;
+}
 
 const meta: Meta = {
   title: 'Welcome',
@@ -19,7 +22,7 @@ const meta: Meta = {
 
 export default meta;
 
-const Template: Story<Props> = args => <Thing {...args} />;
+const Template: Story<{}> = args => <Thing {...args} />;
 
 // By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
 // https://storybook.js.org/docs/react/workflows/unit-testing
