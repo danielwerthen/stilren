ec1f61afd60d735aa611647b70861ecc30b9b99b
Daniel Lichtenstein <danielwerthen@gmail.com>
2021-02-16 20:27:49 +0100

    Add the last references

--- jsx-dev-runtime.js
+++ jsx-dev-runtime.js
@@ -4,8 +4,8 @@ const { jsx } = require('./dist');
 
 const base =
   process.env.NODE_ENV === 'production'
-    ? require('react/cjs/react-jsx-dev-runtime.production.min.js')
-    : (module.exports = require('react/cjs/react-jsx-dev-runtime.development.js'));
+    ? require('base-react/cjs/react-jsx-dev-runtime.production.min.js')
+    : (module.exports = require('base-react/cjs/react-jsx-dev-runtime.development.js'));
 
 module.exports = {
   ...base,
